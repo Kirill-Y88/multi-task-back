475be6d55cdbe387a890a821ec52a8023a52c3cb
KirillPushkin <benberov09@mail.ru>
2022-12-05 12:43:33 +0300

    Commit#1 05.12.2022

---
+++ src/main/java/y88/kirill/multitaskback/MultiTaskBackApplication.java
@@ -0,0 +1,13 @@
+package y88.kirill.multitaskback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTaskBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTaskBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/y88/kirill/multitaskback/MultiTaskBackApplicationTests.java
@@ -0,0 +1,13 @@
+package y88.kirill.multitaskback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTaskBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
